3654421b0079d6897be478c26274b7915b088fcd
Sebastian <sebastianfonck1@gmail.com>
2020-08-06 12:19:59 -0500

    Server. Creation of the console server. This will be the server side of our solution.

---
+++ Server/ServerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
